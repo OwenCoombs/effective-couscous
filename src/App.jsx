637d5ceec957c9ed2e427cee02a7b991e362ff6c
Owen Coombs <owencoombs03@gmail.com>
2024-06-24 11:47:58 -0400

    new stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import { Link } from "react-router-dom"
 
-const Title = () => {
-  return (
-    <h1>
-      Hello World!
-    </h1>
-  )
-}
+
 
 function App() {
   return (
-    <div className="p-5">
-      <Link to='/about'>About</Link>
-      <Title />
+  <div>
+      <div className="d-flex justify-content-center mt-1">
+    <Link to={"/projects"}>
+      <h1 id="projects">Projects</h1>
+    </Link>
     </div>
+      <div className="d-flex justify-content-center mt-1">
+    <Link to={"/Contact"}>
+      <h1 id="projects">Contact</h1>
+    </Link>
+    </div>
+      <div className="d-flex justify-content-center mt-1">
+      <Link to="/Aboutme">
+      <h1 id="projects">About</h1>
+    </Link>
+    </div>
+    <div className="d-flex justify-content-center" id="h6left">
+      <h5 id="h6font">Owen Coombs</h5>
+    </div>
+      <div className="d-flex justify-content-center" id="h6left">
+        <p>Full-Stack Software Engineer</p>
+      </div>
+      
+  </div>
   )
 }
 
