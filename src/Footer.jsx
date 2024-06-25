aa8735971e08105654fd545dd9223475fdd396e9
Owen Coombs <owencoombs03@gmail.com>
2024-06-25 09:47:44 -0400

    dkdfgl

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,6 +6,9 @@ function Footer() {
       <div className="container">
         <p>Footer Content</p>
       </div>
+      <div className="container">
+        <p>Email: Owencoombs03@gmail.com</p>
+      </div>
     </footer>
   );
 }
--- src/contact.jsx
+++ src/contact.jsx
@@ -3,14 +3,14 @@ import linkdin from "./assets/linkedin.svg";
 import email from "./assets/envelope.svg";
 import facebook from "./assets/facebook.svg";
 
-function Contact(){
-    return(
+function Contact() {
+    return (
         <div>
             <div className="text-center container border border-black rounded">
                 <img src={Logo} alt="Logo" />
                 <div className="row">
                     <div className="col">
-                        <a href="https://www.linkedin.com/in/owen-coombs-ab029b254/" target="_blank" rel="noopener noreferrer" id="linkdinpic">
+                        <a href="www.linkedin.com/in/owen-coombs" target="_blank" rel="noopener noreferrer" id="linkdinpic">
                             <img src={linkdin} id="linkdin" alt="LinkedIn" />
                         </a>
                     </div>
